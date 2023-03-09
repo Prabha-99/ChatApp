62d64409f47d9e5ca289d60cc8f7cc31110f3b98
Prabhashana Wijesinghe <103393651+Prabha-99@users.noreply.github.com>
2023-03-09 11:00:38 +0530

    Added some Dummy Classes and Resources for the sake of Uploading Packages into Github

---
+++ src/Logic/testLogic.java
@@ -0,0 +1,12 @@
+
+package Logic;
+
+
+public class testLogic {
+    public static void main(String[] args) {
+        System.out.println("...All the Program Logics goes here....And this is just a tempory Class..");
+        System.out.println("You must have to include hibernate into this project in order to perform CRUD operations with MYSQL Backend");
+        System.out.println("There is folder call 'Resources'. You can use that folder to put any project related stuff like icons,images ");
+    }
+    
+}
